11297582d1c012c82d3a208ad698147a7c6632eb
Prabin Adhikari <adhikari.prabin10@gmail.com>
2016-11-11 23:06:07 -0600

    add: Sprint entity is added

---
+++ src/main/java/edu/awesome/mumscrum/domain/Sprint.java
@@ -0,0 +1,68 @@
+/**
+ * 
+ */
+package edu.awesome.mumscrum.domain;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Transient;
+
+/**
+ * @author prabinadhikari
+ *
+ */
+@Entity
+public class Sprint {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	@Transient
+	private List<Story> stories;
+	private Date startDate;
+	private Date endDate;
+
+	/**
+	 * 
+	 */
+	public Sprint() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public List<Story> getStories() {
+		return stories;
+	}
+
+	public void setStories(List<Story> stories) {
+		this.stories = stories;
+	}
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+	public Date getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+
+}
