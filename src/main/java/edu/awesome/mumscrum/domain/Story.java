7ae7c8fdf0be8032bb8ad0f5797c9e2732ffd448
sabin <nibaserc@gmail.com>
2016-11-11 22:49:08 -0600

    setup: domain and service packages are added

---
+++ src/main/java/edu/awesome/mumscrum/domain/Story.java
@@ -0,0 +1,5 @@
+package edu.awesome.mumscrum.domain;
+
+public class Story {
+
+}
---
+++ src/main/java/edu/awesome/mumscrum/service/ServiceResponse.java
@@ -0,0 +1,49 @@
+package edu.awesome.mumscrum.service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ServiceResponse {
+	private Boolean success;
+	private String message;
+	private Map<String, Object> params;
+
+	public ServiceResponse(String message) {
+		this(true, message);
+	}
+
+	public ServiceResponse(Boolean success, String message) {
+		this.success = success;
+		this.message = message;
+		this.params = new HashMap<String, Object>();
+	}
+
+	public void addParam(String key, Object value) {
+		params.put(key, value);
+	}
+
+	public Boolean getSuccess() {
+		return success;
+	}
+
+	public void setSuccess(Boolean success) {
+		this.success = success;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public Map<String, Object> getParams() {
+		return params;
+	}
+
+	public void setParams(Map<String, Object> params) {
+		this.params = params;
+	}
+
+}
